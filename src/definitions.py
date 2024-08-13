09fab35658edc408c71245d5f7fb0568c717ebeb
Jay/Fienna Liang <roackb2@gmail.com>
2024-08-13 22:22:06 +0800

    Define global values and write a simple csv file

---
+++ src/definitions.py
@@ -0,0 +1,4 @@
+import os
+
+ROOT_PATH = os.path.abspath(__package__)
+DATA_DIR = os.path.join(ROOT_PATH, 'data')
--- src/playground/playground.py
+++ src/playground/playground.py
@@ -1,7 +1,20 @@
+import os
+from definitions import DATA_DIR
+
+def write_csv():
+  house_dir_path = os.path.join(DATA_DIR, 'house_tiny.csv')
+  print(f"Writing house values to {house_dir_path}")
+  with open(house_dir_path, 'w') as file:
+    file.write('''NumRooms,RoofType,Price
+NA,NA,127500
+2,NA,106000
+4,Slate,178100
+NA,NA,140000
+''')
 
 def scratch_pad():
-  print(f"scratch pad")
-  pass
+  print(f"running scratch pad")
+  write_csv()
 
 def run():
   try:
